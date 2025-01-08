e5ced0ceeea36aa27fdf70a6ece951c76bdeb2ab
Windowsxpcoder <windowsxplora@gmail.com>
2025-01-08 08:39:55 +0100

    Update status.ts

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -2,7 +2,7 @@ export const checkStatus = async (interaction) => {
     const { getMinecraftStatus } = require('../utils/minecraftStatus');
 
     try {
-        const status = await getMinecraftStatus('wings2.mrserver.ddnss.de', 25612);
+        const status = await getMinecraftStatus('Minecraft server IP', 'port');
         await interaction.reply(`Server Status: ${status.online ? 'Online' : 'Offline'}\nPlayer Count: ${status.players.online}/${status.players.max}`);
     } catch (error) {
         console.error('Error fetching server status:', error);
